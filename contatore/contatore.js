2dfcebcc8664a8b0e4b15ec61e4c4a015b9769db
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-14 15:31:16 +0100

    aggiornamenti

--- contatore/contatore.js
+++ contatore/contatore.js
@@ -1,3 +1,18 @@
+//  primo esercizio
+
+const save = document.getElementById('save')
+const del = document.getElementById('delete')
+const input = document.getElementById('nome')
+
+save.addEventListener("click", function(){
+    localStorage.setItem("nome:", input.value);
+    console.log(input.value);
+    localStorage.getItem(input.value)
+})
+
+
+// secondo esercizio
+
 let p = document.getElementById('counter')
 let counterValue = parseInt(sessionStorage.getItem("lastCounterValue")) || 0;
 
